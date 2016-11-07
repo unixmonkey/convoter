f1d24dcaabbcf2881bb58a6030b9fb4882c327c1
David Jones <david@getfretless.com>
2016-11-07 16:08:55 -0500

    Add current_user and ability to log in/out.
    Following directions at:
    https://richonrails.com/articles/google-authentication-in-ruby-on-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  get 'sessions/destroy'
-
-  get 'home/show'
-
+  resources :sessions, only: [:create, :destroy]
   resources :conferences
+
+  resource :home, only: [:show]
+
+  root to: 'home#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
