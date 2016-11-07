2acb0c0d227a5904655fdd7cf28c437000bd7e56
David Jones <david@getfretless.com>
2016-11-07 17:36:19 -0500

    Add Conference#daynames & reduce duplication.

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,4 +1,10 @@
 class Conference < ApplicationRecord
   has_many :slots
   scope :upcoming, -> { where arel_table[:ends_at].gt(Time.now) }
+
+  def daynames
+    (starts_at.to_i..ends_at.to_i).step(1.day.to_i).map do |second|
+      Date::DAYNAMES[Time.at(second).wday]
+    end
+  end
 end
