b3f9cc43bf16fde6975860886723210bed01ddf8
David Jones <david@getfretless.com>
2016-11-07 15:52:54 -0500

    Add home page and sessions controller.
    `bin/rails g controller home show
    bin/rails g controller Sessions create destroy`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'home/show'
+
   resources :conferences
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
