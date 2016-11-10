01b4c7951cf21f5992d68afe2422c1e380ef6795
David Jones <david@getfretless.com>
2016-11-10 11:44:33 -0500

    Bring user channel back.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
+    cookies.signed['user.id'] = user.id
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
+    cookies.signed['user.id'] = nil
     redirect_to root_path
   end
 end
--- app/jobs/vote_job.rb
+++ app/jobs/vote_job.rb
@@ -6,7 +6,7 @@ class VoteJob < ApplicationJob
     slot = vote.talk.slot
     html = ConferencesController.render slot,
       locals: { current_user: vote.user }
-    ActionCable.server.broadcast "votes_channel",
+    ActionCable.server.broadcast "votes_#{user_id}_channel",
       message: 'votes_updated',
       slot_id: slot.id,
       talk_id: talk_id,
