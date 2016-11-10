c9681773b3daac37ef29574f531c64569caa951a
David Jones <david@getfretless.com>
2016-11-10 09:23:30 -0500

    Remove votes for other talks in the same slot.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,13 @@
 class VotesController < ApplicationController
   def create
-    current_user.votes.where(talk_id: params[:talk_id]).first_or_create
-    # ActionCable.server.broadcast "votes_#{params[:talk_id]}_channel",
-    #                              message: 'votes_updated'
+    talk = Talk.find(params[:talk_id])
+    talk.slot.votes.where(user_id: current_user.id).destroy_all
+    current_user.votes.create(talk_id: params[:talk_id])
     head :ok
   end
 
   def destroy
     current_user.votes.where(talk_id: params[:talk_id]).destroy_all
-    # ActionCable.server.broadcast "votes_#{params[:talk_id]}_channel",
-    #                              message: 'votes_updated'
     head :ok
   end
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,4 +1,5 @@
 class Slot < ApplicationRecord
   belongs_to :conference
   has_many :talks
+  has_many :votes, through: :talks
 end
