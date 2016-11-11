4fb35c062800f0432e0729a106596a8598c26321
David Jones <david@getfretless.com>
2016-11-11 11:51:00 -0500

    Order talks ascending.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,7 +2,7 @@ class Talk < ApplicationRecord
   belongs_to :slot
   has_many :votes
 
-  default_scope { order(created_at: :desc) }
+  default_scope { order(created_at: :asc) }
 
   def voter_names
     votes.includes(:user).map { |vote| vote.user.name }.uniq.sort.to_sentence
