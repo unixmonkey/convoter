a6dcf3accc67fee5df866be1eb94d8029a5d508e
David Jones <unixmonkey1@gmail.com>
2017-11-15 19:18:31 -0600

    Make selected day track current day during con.

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -11,4 +11,10 @@ class Conference < ApplicationRecord
   def slots_by_day
     slots.to_a.group_by { |slot| slot.name.reverse.split(' ', 4).last.reverse }
   end
+
+  def current_day_index?(index)
+    start_times = (starts_at.to_i..ends_at.to_i).step(1.day.to_i).map{ |second| Time.at(second) }
+    today_index = start_times.map(&:today?).index(true)
+    today_index == (index + 1)
+  end
 end
