3149323673308fc0c6e127ab8c5bd9641957d794
Miles Z. Sterrett <miles@mileszs.com>
2018-10-31 08:47:39 -0400

    Add latest schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111161759) do
+ActiveRecord::Schema.define(version: 2016_11_11_161759) do
 
   create_table "conferences", force: :cascade do |t|
-    t.text     "description"
-    t.string   "name",        limit: 255
-    t.string   "year",        limit: 4
+    t.text "description"
+    t.string "name", limit: 255
+    t.string "year", limit: 4
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "slots", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "conference_id"
+    t.string "name"
+    t.integer "conference_id"
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["conference_id"], name: "index_slots_on_conference_id"
   end
 
   create_table "talks", force: :cascade do |t|
-    t.integer  "slot_id"
-    t.string   "title"
-    t.string   "speaker"
-    t.text     "speaker_detail"
-    t.text     "synopsis"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.string   "location"
+    t.integer "slot_id"
+    t.string "title"
+    t.string "speaker"
+    t.text "speaker_detail"
+    t.text "synopsis"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["slot_id"], name: "index_talks_on_slot_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "oauth_token"
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "talk_id"
+    t.integer "user_id"
+    t.integer "talk_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["talk_id"], name: "index_votes_on_talk_id"
